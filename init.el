4f2aa88a2b1823a88a8830cfa01fea01f62a023b
Ismaïl Boukhssibi <iboukhss@student.42luxembourg.lu>
2024-07-15 15:16:15 +0200

    Added 42 header scripts

--- init.el
+++ init.el
@@ -1,2 +1,11 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
+
+(setq warning-minimum-level :error)
+
+(add-to-list 'load-path (concat user-emacs-directory "ft-lisp"))
+(require 'header)
+(global-set-key (kbd "C-c h") 'header-insert)
